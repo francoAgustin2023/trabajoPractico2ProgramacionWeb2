29005217f6d31cac06ba6a9b617bcaec67360e82
francoAgustin07 <faluque@udc.edu.ar>
2024-09-10 15:31:56 -0300

    Creación del modelo

--- mensajes/models.py
+++ mensajes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Mensaje(models.Model):
+    EMISORES_POSIBLES = [
+        ('juan', 'Juan'),
+        ('maria', 'Maria'),
+    ]
+
+    emisor = models.CharField(max_length=5, choices=EMISORES_POSIBLES)
+    receptor = models.CharField(max_length=5, choices=EMISORES_POSIBLES)
+    contenido = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.emisor} a {self.receptor}: {self.contenido}'
